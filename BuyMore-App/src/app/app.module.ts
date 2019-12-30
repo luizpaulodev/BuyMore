833376b00a07f31e247515e613e484b988071940
Luiz Paulo de Oliveira <luizpaulodev@gmail.com>
2019-12-29 23:07:19 -0300

    Criação de componentes

--- BuyMore-App/src/app/app.module.ts
+++ BuyMore-App/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProdutoComponent } from './produto/produto.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      ProdutoComponent
+      ProdutoComponent,
+      CadastroComponent,
+      LoginComponent
    ],
    imports: [
       BrowserModule,
