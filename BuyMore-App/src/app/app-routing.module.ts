c7dc71bc8a5b50682f5aaf87175f105109e8430b
Luiz Paulo de Oliveira <luizpaulodev@gmail.com>
2019-12-29 22:27:58 -0300

    ProdutoComponent

--- BuyMore-App/src/app/app-routing.module.ts
+++ BuyMore-App/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProdutoComponent } from './produto/produto.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'produto', component: ProdutoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
